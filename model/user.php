79449a21883cbaf2e628d1141202fb2c134406fb
Ryww0 <laurent.chaitas@gmail.com>
2022-09-09 09:11:21 +0200

    construct User class

--- model/user.php
+++ model/user.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace model;
+
+class User
+{
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $sport;
+    private string $password;
+
+    public function __construct(string $name, string $email, string $sport, string $password)
+    {
+        $this->id = uniqid();
+        $this->name = $name;
+        $this->email = $email;
+        $this->sport = $sport;
+        $this->password = $password;
+    }
+}
