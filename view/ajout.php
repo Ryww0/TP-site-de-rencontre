4a071e387164f9b6c9372ccbc4b634018cba6448
Mathieu Molina <molina.mathieu@hotmail.com>
2022-09-09 11:55:15 +0200

    foreach OK

--- view/ajout.php
+++ view/ajout.php
@@ -2,64 +2,85 @@
 include '../view/header.php';
 ?>
 
-<form action="ajout-user.php" method="post">
+    <form action="ajout-user.php" method="post">
 
-    <div>
-        <label for="prenom">Entrez votre Prénom: </label>
-        <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
-    </div>
+        <!--    Première zone   -->
 
-    <div>
-        <label for="nom">Entrez votre Nom: </label>
-        <input type="text" name="nom" id="nom" placeholder="Nom" required>
-    </div>
+        <div>
+            <label for="prenom">Entrez votre Prénom: </label>
+            <input type="text" name="prenom" id="prenom" placeholder="Prénom" required>
+        </div>
 
-    <div>
-        <label for="departement">Entrez votre Département: </label>
-        <input type="number" name="departement" id="departement" min="1" max="96" placeholder="Votre département" required>
-    </div>
+        <div>
+            <label for="nom">Entrez votre Nom: </label>
+            <input type="text" name="nom" id="nom" placeholder="Nom" required>
+        </div>
 
-    <div>
-        <label for="email">Entrez votre e-mail:</label>
-        <input id="email" name="email" type="email">
-    </div>
+        <div>
+            <label for="departement">Entrez votre Département: </label>
+            <input type="number" name="departement" id="departement" min="1" max="96" placeholder="Votre département"
+                   required>
+        </div>
+
+        <div>
+            <label for="email">Entrez votre e-mail:</label>
+            <input type="email" name="email" id="email">
+        </div>
 
 
+        <!--    Seconde zone    -->
+        <br><br>
+        <div>
+            <select name="sport" id="sport">
+                <option value="" disabled selected hidden> Choisissez votre sport</option>
 
+                <?php if (isset($sports) && (!empty($sports))) {
+                    foreach ($sports as $sport) { ?>
+                        <option value="<?= $sport->id ?>"> <?= $sport->name ?> </option>
+                    <?php }
+                } ?>
 
-    <form action="ajout-sport" method="post">
-        <div
-            <select name="sport" id="sport" >
-            <option value="" disabled selected hidden> Choisissez votre sport: </option>
-
-            <?php
-            if (isset($sport) (!empty($sport))){
-            foreach ($sports as $sport) { ?>
-                <option><?php echo $sport ?></option>
-            <?php } ?>
 
             </select>
         </div>
 
+        <!--    Nouveau sport   -->
+        <br><br>
+
+        <form action="ajout-sport" method="post">
+            <div>
+                <label for="autre">Entrez un autre sport:</label>
+                <input type="text" name="autre" id="autre">
+            </div>
+            <div>
+                <input type="submit" value="Ajouter Sport">
+            </div>
+        </form>
+
+        <!--    Niveau   -->
+        <br><br>
         <div>
-            <input type="submit" value="Nouveau Sport">
+            <select name="niveau" id="niveau">
+                <option value="supporter">Supporter</option>
+                <option value="debutant">Débutant</option>
+                <option value="confirme">Confirmé</option>
+                <option value="pro">Pro</option>
+            </select>
         </div>
+
+
+        <br><br>
+        <!--    Submit   -->
+
+        <div>
+            <input type="reset" value="Reset">
+        </div>
+
+        <div>
+            <input type="submit" value="Inscription">
+        </div>
+
     </form>
-
-
-
-    <select name="niveau" id="niveau">
-        <option value="" disabled selected hidden> Choisissez votre niveau:</option>
-
-        <option value="supporter">Supporter</option>
-        <option value="debutant">Débutant</option>
-        <option value="confirme">Confirmé</option>
-        <option value="pro">Pro</option>
-    </select>
-
-
-
-</form>
 
 <?php
 include '../view/footer.php';
