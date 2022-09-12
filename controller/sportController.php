ee0268df923f15f6e659121755dbd040099d6b3e
Ryww0 <laurent.chaitas@gmail.com>
2022-09-12 16:50:17 +0200

    sportController invoke() used a bit

--- controller/sportController.php
+++ controller/sportController.php
@@ -3,17 +3,11 @@
 namespace App\Controller\Back;
 
 use model\Sport;
-// use App\Form\FormSport;   je sais pas quel form ?
 use repository\ISportRepository;
 use repository\SportRepository;
-// use App\Service\Input; On a pas input ?
-// use App\Service\Redirect; On a pas redirect ?
-use service\View;
-use service\Validation;
 
 class SportController
 {
-    use View;
 
     private ISportRepository $sportRepository;
 
@@ -22,69 +16,70 @@ class SportController
         $this->sportRepository = new SportRepository();
     }
 
-    public function invoke(): string
+    public function invoke()
     {
-        return $this->render(
-            SITE_NAME . ' - Sport',
-            'back/pages/sport/index.php',   // Il faut changer cette ligne
-            [
-                'sports' => $this->sportRepository->findAll()
-            ]);
+        return $this->sportRepository->findAll();
+        // return $this->render(
+        //     SITE_NAME . ' - Sport',
+        //     'back/pages/sport/index.php',   // Il faut changer cette ligne
+        //     [
+        //         'sports' => $this->sportRepository->findAll()
+        //     ]);
     }
 
     public function getSportById($params)
     {
-        $sport = $this->sportRepository->findById($params);
-        return $this->render(
-            SITE_NAME . ' - Sport: ' . $sport->design,
-            'back/pages/sport/show.php',
-            [
-                'formDeleteSport' => FormSport::buildDeleteFormWithSport($sport),
-                'formSport' => FormSport::buildUpdateFormWithSport($sport),
-                'sports' => $this->sportRepository->findAll()
-            ]);
+        // $sport = $this->sportRepository->findById($params);
+        // return $this->render(
+        //     SITE_NAME . ' - Sport: ' . $sport->design,
+        //     'back/pages/sport/show.php',
+        //     [
+        //         'formDeleteSport' => FormSport::buildDeleteFormWithSport($sport),
+        //         'formSport' => FormSport::buildUpdateFormWithSport($sport),
+        //         'sports' => $this->sportRepository->findAll()
+        //     ]);
     }
 
     public function deleteSportById($params)
     {
-        $sport = $this->sportRepository->findById($params);
-        $this->sportRepository->remove($sport);
-        Redirect::to('admin/sport');
+        // $sport = $this->sportRepository->findById($params);
+        // $this->sportRepository->remove($sport);
+        // Redirect::to('admin/sport');
     }
 
     public function updateSportById($params)
     {
-        if (Input::exists()) {
-            $val = new Validation;
-            $val->name('design')->value(Input::get('design'))->pattern('alpha')->required();
-            if ($val->isSuccess()) {
-                $sport = $this->sportRepository->findById($params);
-                $sport->setDesign(Input::get('design'));
-                $this->sportRepository->update($sport);
-                Redirect::to('admin/sport');
-            }
-        }
+        // if (Input::exists()) {
+        //     $val = new Validation;
+        //     $val->name('design')->value(Input::get('design'))->pattern('alpha')->required();
+        //     if ($val->isSuccess()) {
+        //         $sport = $this->sportRepository->findById($params);
+        //         $sport->setDesign(Input::get('design'));
+        //         $this->sportRepository->update($sport);
+        //         Redirect::to('admin/sport');
+        //     }
+        // }
     }
 
     public function addSport()
     {
-        if (Input::exists()) {
-            var_dump($_POST);
-            $val = new Validation;
-            $val->name('design')->value(Input::get('design'))->pattern('alpha')->required();
-            if ($val->isSuccess()) {
-                $design = Input::get('design');
-                $sport = new Sport($design);
-                $this->sportRepository->add($sport);
-                Redirect::to('admin/sport');
-            }
-        } else {
-            return $this->render(
-                SITE_NAME . ' - Add Sport: ',
-                'back/pages/sport/new.php',
-                [
-                    'formSport' => FormSport::buildCreateForm(),
-                ]);
-        }
+        //     if (Input::exists()) {
+        //         var_dump($_POST);
+        //         $val = new Validation;
+        //         $val->name('design')->value(Input::get('design'))->pattern('alpha')->required();
+        //         if ($val->isSuccess()) {
+        //             $design = Input::get('design');
+        //             $sport = new Sport($design);
+        //             $this->sportRepository->add($sport);
+        //             Redirect::to('admin/sport');
+        //         }
+        //     } else {
+        //         return $this->render(
+        //             SITE_NAME . ' - Add Sport: ',
+        //             'back/pages/sport/new.php',
+        //             [
+        //                 'formSport' => FormSport::buildCreateForm(),
+        //             ]);
+        //     }
     }
 }
--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 
 include('./config.php');
 
+include('./controller/sportController.php');
 include('./repository/sportRepository.php');
 // include('./repository/IsportRepository.php');
 include('./model/sport.php');
 
+use App\Controller\Back\SportController;
 use repository\SportRepository;
 
 
 $test = new SportRepository();
 var_dump($test->findAll());
+
+echo '<br><br>';
+echo '--------------------------';
+echo '<br><br>';
+
+$test2 = new SportController();
+var_dump($test2->invoke());
