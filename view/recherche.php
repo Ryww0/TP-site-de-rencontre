9a3737d36c65f007e9b5d042ee6433bcfa3f4680
kava <wabutrune.p@hotmail.com>
2022-09-12 14:46:34 +0200

    recherche 0.2

--- view/recherche.php
+++ view/recherche.php
@@ -1,5 +1,44 @@
 <?php
 include '../view/header.php';
+?>
+
+
+<form action="recherche.php" method="post">
+
+
+
+    <label for="sport-select">Choisissez un sport:</label>
+
+    <div>
+        <select name="sport" id="sport">
+            <option value=  <?php $sport ?>   ></option>
+        </select>
+    </div>
+
+    <br><br>
+
+
+    <label for="niveau-select">Choisissez le niveau:</label>
+    <div>
+            <select name="niveau" id="niveau">
+                <option value="supporter">Supporter</option>
+                <option value="debutant">DÃ©butant</option>
+                <option value="confirme">ConfirmÃ©</option>
+                <option value="pro">Pro</option>
+            </select>
+    </div>
+
+
+<!--    Reset/Submit   -->
+    <div>
+        <input type="reset" value="Reset">
+    </div>
+
+    <div>
+        <input type="submit" value="Lancer la recherche">
+    </div>
+
+</form>
 
 
 
@@ -7,19 +46,5 @@ include '../view/header.php';
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
 include '../view/footer.php';
