866b40c43ab79844540f7b6600e034553f21be11
Ryww0 <laurent.chaitas@gmail.com>
2022-09-12 13:32:00 +0200

    display all sports in db

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 include('./view/partials/head.php');
 
+$sports = new SportRepository();
 
+foreach ($sports->findAll() as $sport) {
+    echo $sport;
+}
 
 include('./view/partials/footer.php');
